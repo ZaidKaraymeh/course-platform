fdb025b6b0bdb6d7854ef5c0beb6f7f5e888d429
Zaid <karaymehzaid@gmail.com>
2022-09-16 13:08:44 +0300

    0.0.3 Rating Helper Functions Tests

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import *
+from .utils.helpers import subtract_new_average, add_new_average
+from decimal import Decimal 
+
+class RatingTests(TestCase):
+    
+    def test_add_new_average(self):
+        avg = 4.7
+        count = 3
+        value = 3
+        expected_avg = Decimal('4.3')
+        new_average = add_new_average(avg, count, value)
+        self.assertEqual(expected_avg, new_average)
+        self.assertIsInstance(new_average, Decimal)
+
+    def test_subtract_new_average(self):
+        avg = 4.3
+        count = 4
+        value = 3
+        expected_avg = Decimal('4.7')
+        new_average = subtract_new_average(avg, count, value)
+        self.assertEqual(expected_avg, new_average)
+        self.assertIsInstance(new_average, Decimal)
--- courses/utils/helpers.py
+++ courses/utils/helpers.py
@@ -2,15 +2,15 @@ from decimal import Decimal
 import math as m
 def add_new_average(rating_average, count, value):
     avg = round(((count*rating_average) + value)/(count + 1), 1)
-    rounded = m.ceil(avg * 10) / 10
+    #rounded = m.ceil(avg * 10) / 10
     #avg2 = rating_average + ((value - rating_average)/count)
 
-    return Decimal(f'{rounded}')
+    return Decimal(f'{avg}')
 
 def subtract_new_average(rating_average, count, value):
-    avg = ((rating_average * count) - value) / (count - 1)
-    rounded = m.ceil(avg * 10) / 10
-    return Decimal(f'{rounded}')
+    avg = round(((rating_average * count) - value) / (count - 1), 1)
+    #rounded = m.ceil(avg * 10) / 10
+    return Decimal(f'{avg}')
     
 #avg = add_new_average(4.5, 2, 4)
 #print(avg)
